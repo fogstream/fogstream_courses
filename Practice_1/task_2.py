74ae47c0bc14e13d25c17bd1439cd7513fe13d54
Egor Proskuryakov <egor.proskuryakov@gmail.com>
2018-10-15 15:54:18 +1000

    Init

---
+++ Practice_1/task_2.py
@@ -0,0 +1,16 @@
+""" 
+Тут условие задачи
+"""
+
+first_number = int(input())
+first_second = int(input())
+
+# Тут будет какой-то код
+# Возможно и тут
+# ...
+# А может даже и тут
+
+
+print(new_first_number)
+print(new_second_number)
+
